dd614dbb085c48140d423bdc518ee98d6f0993ef
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-21 15:35:25 +0100

    update

---
+++ src/components/LanguageButton.jsx
@@ -0,0 +1,9 @@
+function LanguageButton() {
+    return (
+      <button>
+        <h1>Learn Web Development</h1>
+      </button>
+    );
+  }
+  
+  export default LanguageButton;
---
+++ src/components/LanguageCard.jsx
@@ -0,0 +1,9 @@
+function LanguageCard() {
+    return (
+      <button>
+        <h1>Learn Web Development</h1>
+      </button>
+    );
+  }
+  
+  export default LanguageCard;
