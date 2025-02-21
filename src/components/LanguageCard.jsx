a6e3d554384ca4223f1077b25ae428f717012c7c
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-21 15:42:11 +0100

    language card

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -1,8 +1,15 @@
-function LanguageCard() {
+function LanguageCard({ selectedLanguage }) {
     return (
-      <button>
-        <h1>Learn Web Development</h1>
-      </button>
+      <div className="language-card">
+        {selectedLanguage ? (
+          <>
+            <h2>{selectedLanguage.title}</h2>
+            <p>{selectedLanguage.description}</p>
+          </>
+        ) : (
+          <p>Nessun linguaggio selezionato</p>
+        )}
+      </div>
     );
   }
   
