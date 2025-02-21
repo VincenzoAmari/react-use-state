a96c2e35ca4b2ce4208da00e2dddefde389667d7
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-21 15:02:07 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import './App.css'
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import LanguageButton from './components/LanguageButton';
+import LanguageCard from './components/LanguageCard';
+import LanguagesDetail from './data/LanguagesDetail';
 
 function App() {
   
