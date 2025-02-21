14b5bad24dc5e23a39c7013056e9056327c89368
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-21 15:26:46 +0100

    avevo sbagliato a nominare i file...

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,18 @@
 
+import LanguageButton from "./LanguageButton";
+import LanguageCard from "./LanguageCard";
+
+function Main({ languages, selectedLanguage, setSelectedLanguage }) {
+  return (
+    <main>
+      <div className="buttons">
+        {languages.map(lang => (
+          <LanguageButton key={lang.id} lang={lang} setSelectedLanguage={setSelectedLanguage} selectedLanguage={selectedLanguage} />
+        ))}
+      </div>
+      <LanguageCard selectedLanguage={selectedLanguage} />
+    </main>
+  );
+}
+
+export default Main;
